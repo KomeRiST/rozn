ef69b778db2112938fa23dca2e920eb2c59265ff
Юрий Комерист <komerist1993-93@mail.ru>
2019-05-27 00:15:40 +0500

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rozn
+{
+    class ApplicationContext : DbContext
+    {
+
+        public ApplicationContext() : base("RoznConnection")
+        {
+        }
+        public DbSet<tovar> Tovars { get; set; }
+
+
+
+
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace rozn
+{
+    class ViewModel:ViewModelBase
+    {
+        ApplicationContext db;
+        public int Clicks { get; set; }
+        public IEnumerable<tovar> Tovary { get; set; }
+
+
+        public ICommand AddClick {
+            get {
+                return new DelegateCommand(() => { Clicks++; });
+            }
+        }
+
+
+        public ViewModel()
+        {
+            db = new ApplicationContext();
+            db.Tovars.Load();
+            Tovary = db.Tovars.Local.ToBindingList();
+        }
+
+
+
+    }
+}
